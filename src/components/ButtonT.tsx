895f04cbafa66389b33cfc8719deb6b163c17069
user <user@users.noreply.github.com>
2024-04-19 13:51:16 +1000

    menu size

--- src/components/ButtonT.tsx
+++ src/components/ButtonT.tsx
@@ -3,14 +3,14 @@ import Link from 'next/link';
 
 const ButtonT: React.FC = () => {
   return (
-    <section className="py-8 px-4 flex justify-center w-[20vw]">
-      <div className="space-x-4">
-        <button className="mt-4 bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
+    <section className="py-8 flex justify-center w-[20vw]">
+      <div className="space-x-2">
+        <button className="mt-4 bg-blue-500 hover:bg-blue-600 text-white font-bold px-2 rounded">
           <Link href="/menu">
             <div className="block cursor-pointer">View Menu</div>
           </Link>
         </button>
-        <button className="mt-4 bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
+        <button className="mt-4 bg-blue-500 hover:bg-blue-600 text-white font-bold px-2 rounded">
           <Link href="/menu-photos">
             <div className="block cursor-pointer">Menu w/ photos</div>
           </Link>
