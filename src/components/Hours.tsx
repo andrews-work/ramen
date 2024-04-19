f143885748553df47a301d5874cbd649a66d2b59
user <user@users.noreply.github.com>
2024-04-19 11:02:55 +1000

    hours fontsize

--- src/components/Hours.tsx
+++ src/components/Hours.tsx
@@ -8,35 +8,33 @@ const Hours: React.FC = () => {
       <div>
         <h3 className="text-xl font-semibold mb-2">Lunch</h3>
         <table className="w-full text-left">
-          <thead>
-          </thead>
           <tbody>
             <tr>
-              <td className="text-base md:text-md">Monday</td>
+              <td className="text-base md:text-md mr-4">Monday</td>
               <td className="text-base md:text-md">Closed</td>
             </tr>
             <tr>
-              <td className="text-base md:text-md">Tuesday</td>
+              <td className="text-base md:text-md mr-4">Tuesday</td>
               <td className="text-base md:text-md">12:00 PM - 3:00 PM</td>
             </tr>
             <tr>
-              <td className="text-base md:text-md">Wednesday</td>
+              <td className="text-base md:text-md mr-4">Wednesday</td>
               <td className="text-base md:text-md">12:00 PM - 3:00 PM</td>
             </tr>
             <tr>
-              <td className="text-base md:text-md">Thursday</td>
+              <td className="text-base md:text-md mr-4">Thursday</td>
               <td className="text-base md:text-md">12:00 PM - 3:00 PM</td>
             </tr>
             <tr>
-              <td className="text-base md:text-md">Friday</td>
+              <td className="text-base md:text-md mr-4">Friday</td>
               <td className="text-base md:text-md">12:00 PM - 3:00 PM</td>
             </tr>
             <tr>
-              <td className="text-base md:text-md">Saturday</td>
+              <td className="text-base md:text-md mr-4">Saturday</td>
               <td className="text-base md:text-md">12:00 PM - 3:00 PM</td>
             </tr>
             <tr>
-              <td className="text-base md:text-md">Sunday</td>
+              <td className="text-base md:text-md mr-4">Sunday</td>
               <td className="text-base md:text-md">12:00 PM - 3:00 PM</td>
             </tr>
           </tbody>
@@ -46,35 +44,33 @@ const Hours: React.FC = () => {
       <div className="mt-8">
         <h3 className="text-xl font-semibold mb-2">Dinner</h3>
         <table className="w-full text-left">
-          <thead>
-          </thead>
           <tbody>
             <tr>
-              <td className="text-base md:text-md">Monday</td>
+              <td className="text-base md:text-md mr-4">Monday</td>
               <td className="text-base md:text-md">Closed</td>
             </tr>
             <tr>
-              <td className="text-base md:text-md">Tuesday</td>
+              <td className="text-base md:text-md mr-4">Tuesday</td>
               <td className="text-base md:text-md">5:30 PM - 9:00 PM</td>
             </tr>
             <tr>
-              <td className="text-base md:text-md">Wednesday</td>
+              <td className="text-base md:text-md mr-4">Wednesday</td>
               <td className="text-base md:text-md">5:30 PM - 9:00 PM</td>
             </tr>
             <tr>
-              <td className="text-base md:text-md">Thursday</td>
+              <td className="text-base md:text-md mr-4">Thursday</td>
               <td className="text-base md:text-md">5:30 PM - 9:00 PM</td>
             </tr>
             <tr>
-              <td className="text-base md:text-md">Friday</td>
+              <td className="text-base md:text-md mr-4">Friday</td>
               <td className="text-base md:text-md">5:30 PM - 12:00 AM</td>
             </tr>
             <tr>
-              <td className="text-base md:text-md">Saturday</td>
               <td className="text-base md:text-md">5:30 PM - 12:00 AM</td>
+              <td className="text-base md:text-md mr-4">Saturday</td>
             </tr>
             <tr>
-              <td className="text-base md:text-md">Sunday</td>
+              <td className="text-base md:text-md mr-4">Sunday</td>
               <td className="text-base md:text-md">5:30 PM - 9:00 PM</td>
             </tr>
           </tbody>
