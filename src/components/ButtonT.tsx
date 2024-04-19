4d8e79bb478337be0a195aaa10162601b5894506
user <user@users.noreply.github.com>
2024-04-19 15:36:00 +1000

    new layout tablet

--- src/components/ButtonT.tsx
+++ src/components/ButtonT.tsx
@@ -4,17 +4,17 @@ import Link from 'next/link';
 const ButtonT: React.FC = () => {
   return (
     <section className="flex justify-center w-[20vw]">
-      <div className="ml-[2vw]">
-        <button className="mt-4 bg-blue-500 hover:bg-blue-600 text-white font-bold rounded p-2">
-          <Link href="/menu">
-            <div className="block cursor-pointer">View Menu</div>
-          </Link>
-        </button>
-        <button className="mt-4 bg-blue-500 hover:bg-blue-600 text-white font-bold p-2 rounded">
-          <Link href="/menu-photos">
-            <div className="block cursor-pointer">Menu w/ photos</div>
-          </Link>
-        </button>
+      <div className="ml-[2vw] space-x-2">
+        <Link href="/menu">
+          <button className="mt-4 bg-blue-500 hover:bg-blue-600 text-white font-bold text-sm rounded p-2">
+            View Menu
+          </button>
+        </Link>
+        <Link href="/menu-photos">
+          <button className="mt-4 bg-blue-500 hover:bg-blue-600 text-white font-bold text-sm rounded p-2">
+            Menu w/ photos
+          </button>
+        </Link>
       </div>
     </section>
   );
