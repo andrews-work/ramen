dccd5e736e15d4285d8b6506639804b57cd79d55
user <user@users.noreply.github.com>
2024-04-19 14:34:30 +1000

    aboutM tablet

--- src/Layout/TabletLayout.tsx
+++ src/Layout/TabletLayout.tsx
@@ -4,7 +4,7 @@ import TGallery from '@/components/TGallery';
 import Hours from '@/components/Hours';
 import ButtonT from '@/components/ButtonT';
 import Location from '@/components/Location';
-import AboutT from '@/components/AboutT';
+import AboutM from '@/components/AboutM';
 import Image from 'next/image';
 
 const TabletLayout: React.FC = () => {
@@ -24,7 +24,7 @@ const TabletLayout: React.FC = () => {
             <Hours />
           </div>
           <div id="about">
-            <AboutT />
+            <AboutM />
           </div>
         </div>
       </div>
