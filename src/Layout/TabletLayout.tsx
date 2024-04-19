9f700a8a731e87eb2c974d3d3c4f1a65a172bdd0
user <user@users.noreply.github.com>
2024-04-19 14:38:42 +1000

    aboutM tablet

--- src/Layout/TabletLayout.tsx
+++ src/Layout/TabletLayout.tsx
@@ -13,7 +13,7 @@ const TabletLayout: React.FC = () => {
       <Header />
       <div className="mt-4 flex"> 
         <Image src="/ramen.jpg" alt="store front" width={500} height={110} />
-        <div className="flex-col w-[20vw]">
+        <div className="flex-col w-[30vw]">
           <div id="menu">
             <ButtonT />
           </div>
