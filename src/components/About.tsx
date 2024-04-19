f3743b8db263ea597c452805ff8d92a5833b737c
user <user@users.noreply.github.com>
2024-04-19 12:15:31 +1000

    about size

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const About: React.FC = () => {
   return (
-    <section className="py-8 px-4">
+    <section className="py-8 px-4 bg-grey-100 w-full h-full">
       <h2 className="text-2xl font-bold mb-4">About Us</h2>
       <p>Lorem ipsum dolor sit </p>
     </section>
