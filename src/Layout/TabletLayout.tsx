f2642514e08fbbbed30c6871f73643a56bea87b9
user <user@users.noreply.github.com>
2024-04-19 13:13:27 +1000

    tablet small layout

--- src/Layout/TabletLayout.tsx
+++ src/Layout/TabletLayout.tsx
@@ -12,7 +12,7 @@ const TabletLayout: React.FC = () => {
     <div className="items-center justify-center min-h-screen">
       <Header />
       <div className="mt-4 flex"> 
-        <Image src="/ramen.jpg" alt="store front" width={800} height={300} />
+        <Image src="/ramen.jpg" alt="store front" />
         <div className="flex-col">
           <div id="menu">
             <Button />
