8db8c92a426f6837fadc376bd973bef59cd3e43f
user <user@users.noreply.github.com>
2024-04-19 14:54:11 +1000

    smaller font tablet

--- src/components/ButtonT.tsx
+++ src/components/ButtonT.tsx
@@ -3,14 +3,14 @@ import Link from 'next/link';
 
 const ButtonT: React.FC = () => {
   return (
-    <section className="py-8 flex justify-center w-[20vw]">
-      <div className="space-x-2">
-        <button className="mt-4 bg-blue-500 hover:bg-blue-600 text-white font-bold px-2 rounded">
+    <section className="flex justify-center w-[20vw]">
+      <div className="">
+        <button className="mt-4 bg-blue-500 hover:bg-blue-600 text-white font-bold rounded">
           <Link href="/menu">
             <div className="block cursor-pointer">View Menu</div>
           </Link>
         </button>
-        <button className="mt-4 bg-blue-500 hover:bg-blue-600 text-white font-bold px-2 rounded">
+        <button className="mt-4 bg-blue-500 hover:bg-blue-600 text-white font-bold  rounded">
           <Link href="/menu-photos">
             <div className="block cursor-pointer">Menu w/ photos</div>
           </Link>
