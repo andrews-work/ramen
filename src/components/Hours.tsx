a6610d270611cafe4be05887d28255723f5cdade
user <user@users.noreply.github.com>
2024-04-19 12:08:09 +1000

    margin hours

--- src/components/Hours.tsx
+++ src/components/Hours.tsx
@@ -6,7 +6,7 @@ const Hours: React.FC = () => {
       <h2 className="text-2xl font-bold mb-4">Hours</h2>
 
       <div>
-        <h3 className="text-xl font-semibold mb-2 ml-[5vw]">Lunch</h3>
+        <h3 className="text-xl font-semibold mb-2">Lunch</h3>
         <table className="w-full text-left">
           <tbody>
             <tr>
@@ -42,7 +42,7 @@ const Hours: React.FC = () => {
       </div>
 
       <div className="mt-8">
-        <h3 className="text-xl font-semibold mb-2 ml-[5vw]">Dinner</h3>
+        <h3 className="text-xl font-semibold mb-2">Dinner</h3>
         <table className="w-full text-left">
           <tbody>
             <tr>
@@ -50,27 +50,27 @@ const Hours: React.FC = () => {
               <td className="text-base md:text-md px-4">Closed</td>
             </tr>
             <tr>
-              <td className="text-base md:text-md px-4">Tuesday</td>
+              <td className="text-base md:text-md">Tuesday</td>
               <td className="text-base md:text-md px-4">5:30 PM - 9:00 PM</td>
             </tr>
             <tr>
-              <td className="text-base md:text-md px-4">Wednesday</td>
+              <td className="text-base md:text-md">Wednesday</td>
               <td className="text-base md:text-md px-4">5:30 PM - 9:00 PM</td>
             </tr>
             <tr>
-              <td className="text-base md:text-md px-4">Thursday</td>
+              <td className="text-base md:text-md">Thursday</td>
               <td className="text-base md:text-md px-4">5:30 PM - 9:00 PM</td>
             </tr>
             <tr>
-              <td className="text-base md:text-md px-4">Friday</td>
+              <td className="text-base md:text-md">Friday</td>
               <td className="text-base md:text-md px-4">5:30 PM - 12:00 AM</td>
             </tr>
             <tr>
-              <td className="text-base md:text-md px-4">Saturday</td>
+              <td className="text-base md:text-md">Saturday</td>
               <td className="text-base md:text-md px-4">5:30 PM - 12:00 AM</td>
             </tr>
             <tr>
-              <td className="text-base md:text-md px-4">Sunday</td>
+              <td className="text-base md:text-md">Sunday</td>
               <td className="text-base md:text-md px-4">5:30 PM - 9:00 PM</td>
             </tr>
           </tbody>
