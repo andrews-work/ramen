27e53beba9f41f7f47863e1f9b2fe9f7dcc83dc9
user <user@users.noreply.github.com>
2024-04-20 19:40:07 +1000

    ml50

--- src/Layout/DesktopLayout.tsx
+++ src/Layout/DesktopLayout.tsx
@@ -16,23 +16,23 @@ const DesktopLayout: React.FC = () => {
       </div>
 
       {/* main */}
-      <div className="h-70vh flex w-full">
+      <div className="h-70vh flex w-full justify-center items-center">
         {/* left */}
-        <div className="h-70vh flex w-full ">
+        <div className="h-70vh flex w-full justify-center items-center">
           {/* left */}
-          <div id="about" className="h-full ml-[50vw]">
+          <div id="about" className="h-full w-1/4 flex justify-center items-center">
             <Button />
             <Location />
             <About />
           </div>
 
           {/* middle */}
-          <div className="w-[50vw] h-full">
+          <div className="w-1/2 h-full flex justify-center items-center">
             <Image src="/ramen.jpg" alt="Desktop Image" width={800} height={300} />
           </div>
 
           {/* right */}
-          <div id="hours" className="w-[25vw] h-full">
+          <div id="hours" className="w-1/4 h-full flex justify-center items-center">
             <Hours />
           </div>
         </div>
