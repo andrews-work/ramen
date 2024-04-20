7ef7a61332f4ea887d6fb69e66d14472043298ea
user <user@users.noreply.github.com>
2024-04-20 19:56:51 +1000

    space evenely

--- src/components/Hours.tsx
+++ src/components/Hours.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Hours: React.FC = () => {
   return (
-    <section className="px-4 mb-8 ml-[15vw]">
+    <section className="px-4 mb-8 ml-[5vw]">
       <h2 className="text-2xl font-bold mb-4 text-blue">Opening Hours</h2>
 
       <div>
