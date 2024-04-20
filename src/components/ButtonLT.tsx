870e751ceb22c782a080132c0dc44615538feed0
user <user@users.noreply.github.com>
2024-04-21 05:56:53 +1000

    tablet large buttons menu

--- src/components/ButtonLT.tsx
+++ src/components/ButtonLT.tsx
@@ -5,12 +5,12 @@ const ButtonLT: React.FC = () => {
   return (
     <section className="pb-2 px-2 flex-col ml-4 justify-center text-xs"> {/* Change text size here */}
       <div>
-        <button className="mt-2 bg-blue-500 hover:bg-blue-600 text-white font-bold py-0.5 px-1 rounded"> {/* Change padding here */}
+        <button className="mt-2 bg-blue-500 hover:bg-blue-600 text-white font-bold py-0.5 px-2 rounded"> {/* Change padding here */}
           <Link href="/menu">
             <div className="block cursor-pointer">View Menu</div>
           </Link>
         </button>
-        <button className="mt-2 bg-blue-500 hover:bg-blue-600 text-white font-bold py-0.5 px-1 rounded"> {/* Change padding here */}
+        <button className="mt-2 bg-blue-500 hover:bg-blue-600 text-white font-bold py-0.5 px-2 rounded"> {/* Change padding here */}
           <Link href="/menu-photos">
             <div className="block cursor-pointer">Menu w/ photos</div>
           </Link>
