7097796af867f7a3d9497580d3b21d34881581b6
user <user@users.noreply.github.com>
2024-04-20 18:32:37 +1000

    remove nav fropm header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,15 +7,6 @@ const Header: React.FC = () => {
       <div className="flex justify-center">
         <h1 className="text-2xl md:text-3xl font-bold text-white mb-4">Manpuku</h1>
       </div>
-      <nav className="flex justify-center space-x-4 text-white">
-        <a className="text-base sm:text-lg md:text-xl lg:text-xl" href="#location">Contact</a>
-        <a className="text-base sm:text-lg md:text-xl lg:text-xl" href="#location">Location</a>
-        <a className="text-base sm:text-lg md:text-xl lg:text-xl" href="#hours">Hours</a>
-        <a className="text-base sm:text-lg md:text-xl lg:text-xl" href="#about">About</a>
-        <Link href="/menu">
-          <div className="text-base sm:text-lg md:text-xl lg:text-2xl">Menu</div>
-        </Link>
-      </nav>
     </header>
   );
 };
