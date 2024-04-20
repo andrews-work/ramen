3f221cb662867157a297f67d17b7e33d127c99e1
user <user@users.noreply.github.com>
2024-04-21 06:15:55 +1000

    mobile layout

--- src/Layout/MobileLayout.tsx
+++ src/Layout/MobileLayout.tsx
@@ -12,7 +12,7 @@ const MobileLayout: React.FC = () => {
   return (
     <div className="items-center justify-center min-h-screen">
       <Header />
-      <div className="mt-4 flex-col items-center"> 
+      <div className="mt-4 flex-col items-center justify-center"> 
         <Image src="/ramen.jpg" alt="store front" width={800} height={300} />
         <div id="menu">
           <Button />
