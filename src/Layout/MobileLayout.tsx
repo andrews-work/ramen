0079af1285f8b41a82ada560b4e6d67626334c84
user <user@users.noreply.github.com>
2024-04-21 06:09:52 +1000

    mobile design

--- src/Layout/MobileLayout.tsx
+++ src/Layout/MobileLayout.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import Header from '@/components/Header';
 import Image from 'next/image';
 import Button from '@/components/Button';
-import Hours from '@/components/Hours';
+import HoursM from '@/components/HoursM';
 import Location from '@/components/Location';
 import AboutT from '@/components/AboutT';
 import MobileGallery from '@/components/MobileGallery';
@@ -12,18 +12,18 @@ const MobileLayout: React.FC = () => {
   return (
     <div className="items-center justify-center min-h-screen">
       <Header />
-      <div className="mt-4"> 
+      <div className="mt-4 ml-4"> 
         <Image src="/ramen.jpg" alt="store front" width={800} height={300} />
-        <div id="menu" className="ml-[2vw]">
+        <div id="menu">
           <Button />
         </div>
-        <div id="location" className="ml-[9vw]">
+        <div id="location">
           <Location />
         </div>
-        <div id="hours" className="ml-[9vw]">
-          <Hours />
+        <div id="hours">
+          <HoursM />
         </div>
-        <div id="about" className="ml-[9vw]">
+        <div id="about">
           <AboutT />
         </div>
         <div id="gallery">
---
+++ src/components/HoursM.tsx
@@ -0,0 +1,83 @@
+import React from 'react';
+
+const HoursM: React.FC = () => {
+  return (
+    <section className="px-4 mb-8">
+      <h2 className="text-2xl font-bold mb-8 text-blue">Opening Hours</h2>
+
+      <div>
+        <h3 className="text-xl font-semibold mb-6 decoration-white underline">Lunch</h3>
+        <table className="w-full text-left">
+          <tbody>
+            <tr>
+              <td className="text-base md:text-md">Monday</td>
+              <td className="text-base md:text-md px-4">Closed</td>
+            </tr>
+            <tr>
+              <td className="text-base md:text-md">Tuesday</td>
+              <td className="text-base md:text-md px-4">12PM - 3 PM</td>
+            </tr>
+            <tr>
+              <td className="text-base md:text-md">Wednesday</td>
+              <td className="text-base md:text-md px-4">12 PM - 3 PM</td>
+            </tr>
+            <tr>
+              <td className="text-base md:text-md">Thursday</td>
+              <td className="text-base md:text-md px-4">12 PM - 3 PM</td>
+            </tr>
+            <tr>
+              <td className="text-base md:text-md">Friday</td>
+              <td className="text-base md:text-md px-4">12 PM - 3 PM</td>
+            </tr>
+            <tr>
+              <td className="text-base md:text-md">Saturday</td>
+              <td className="text-base md:text-md px-4">12 PM - 3 PM</td>
+            </tr>
+            <tr>
+              <td className="text-base md:text-md">Sunday</td>
+              <td className="text-base md:text-md px-4">12 PM - 3 PM</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+
+      <div className="mt-10">
+        <h3 className="text-xl font-semibold mb-6 decoration-white underline">Dinner</h3>
+        <table className="w-full text-left">
+          <tbody>
+            <tr>
+              <td className="text-base md:text-md">Monday</td>
+              <td className="text-base md:text-md px-4">Closed</td>
+            </tr>
+            <tr>
+              <td className="text-base md:text-md">Tuesday</td>
+              <td className="text-base md:text-md px-4">5 PM - 9 PM</td>
+            </tr>
+            <tr>
+              <td className="text-base md:text-md">Wednesday</td>
+              <td className="text-base md:text-md px-4">5 PM - 9 PM</td>
+            </tr>
+            <tr>
+              <td className="text-base md:text-md">Thursday</td>
+              <td className="text-base md:text-md px-4">5 PM - 9 PM</td>
+            </tr>
+            <tr>
+              <td className="text-base md:text-md">Friday</td>
+              <td className="text-base md:text-md px-4">5PM - 12 AM</td>
+            </tr>
+            <tr>
+              <td className="text-base md:text-md">Saturday</td>
+              <td className="text-base md:text-md px-4">5 PM - 12 AM</td>
+            </tr>
+            <tr>
+              <td className="text-base md:text-md">Sunday</td>
+              <td className="text-base md:text-md px-4">5 PM - 9 PM</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </section>
+  );
+};
+
+export default HoursM;
