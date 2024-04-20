12678f85180eecb8152d623d337d6600e4df5110
user <user@users.noreply.github.com>
2024-04-20 15:39:13 +1000

    adjust LT layout

--- src/components/ButtonLT.tsx
+++ src/components/ButtonLT.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 const ButtonLT: React.FC = () => {
   return (
     <section className="pb-2 px-2 flex-col justify-center text-sm">
-      <div className="space-x-2">
+      <div>
         <button className="mt-4 bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-2 rounded">
           <Link href="/menu">
             <div className="block cursor-pointer">View Menu</div>
