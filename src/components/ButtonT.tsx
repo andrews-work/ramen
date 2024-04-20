ef2a21e1d95c7d3b34eb9c4b4ba26b572093d282
user <user@users.noreply.github.com>
2024-04-20 15:19:56 +1000

    about lt

--- src/components/ButtonT.tsx
+++ src/components/ButtonT.tsx
@@ -4,14 +4,14 @@ import Link from 'next/link';
 const ButtonT: React.FC = () => {
   return (
     <section className="flex flex-row justify-center w-[30vw] mt-3">
-      <div className="ml-[2vw] space-x-4">
+      <div className="ml-[2vw] space-x-3">
         <Link href="/menu">
-          <button className="mt-4 bg-blue-500 hover:bg-blue-600 text-white font-bold text-sm rounded p-2">
+          <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold text-sm rounded p-1">
             View Menu
           </button>
         </Link>
         <Link href="/menu-photos">
-          <button className="mt-4 bg-blue-500 hover:bg-blue-600 text-white font-bold text-sm rounded p-2">
+          <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold text-sm rounded p-1">
             Menu w/ photos
           </button>
         </Link>
