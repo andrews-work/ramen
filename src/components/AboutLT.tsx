54934e58a27ed6e98a26f9d592f8c2a836ba610d
user <user@users.noreply.github.com>
2024-04-20 15:16:46 +1000

    about lt

--- src/components/AboutLT.tsx
+++ src/components/AboutLT.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const AboutLT: React.FC = () => {
   return (
-    <section className="py-8 px-4 w-[90vw] h-full">
+    <section className="py-8 px-4 w-[20vw] h-full">
       <h2 className="text-xl font-bold mb-4">About Us</h2>
       <p className="text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos mollitia adipisci libero unde illo reprehenderit atque, eligendi vero inventore modi qui asperiores ducimus dolorum tempore aliquam ex quas, est ea. </p>
     </section>
